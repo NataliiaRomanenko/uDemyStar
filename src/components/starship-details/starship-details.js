1a332fb70c4c248b82548b570255a3e06241e9ce
Nataly Romanenko <nataliia.romanenko@gmail.com>
2019-07-25 16:57:13 +0300

    start project uDemuStar. Create components

---
+++ src/components/starship-details/starship-details.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './starship-details.css';
+
+export default class StarshipDetails extends Component {
+    render() {
+        return(
+            <div className="starship-details"></div>
+        )
+    }
+
+}
